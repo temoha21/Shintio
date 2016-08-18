795e91492fc0e22e6c2d201b3ea92735a74dc50a
temoha21 <temoha21@live.ru>
2016-08-18 16:51:37 +0300

    Added base controlle,added base view,added mongodb

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var app = require(__dirname + '/app/app')
-  , port = process.env.PORT || 3000
-
-app.listen(port, function () {
-  console.log('Listening on port ', port)
-})
+var config = require(__dirname + '/config')();
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://127.0.0.1:27017/Shintio', function(err, db) {
+    if(err) {
+        console.log('Sorry, there is no mongo db server running.');
+    } else {
+        var attachDB = function(req, res, next) {
+            req.db = db;
+            next();
+        };
+        var app = require(__dirname + '/app/app')
+        , port = config.port;
+        app.listen(port, function () {
+            console.log('Listening on port ', port)
+        })
+    }
+});
